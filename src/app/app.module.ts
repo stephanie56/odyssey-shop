c0262ffeada08a282507279fd1edf917ea421a81
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-12-27 21:56:08 -0500

    #2 Integrate with product api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,7 +8,12 @@ import { ProductPageModule } from "./product-page/product-page.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductPageModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ProductPageModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { ApiService } from "../shared/services/api.service";
+import { Observable } from "rxjs";
+import { delay } from "rxjs/operators";
 
 @Component({
-  selector: 'os-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.scss'],
+  selector: "os-product-page",
+  templateUrl: "./product-page.component.html",
+  styleUrls: ["./product-page.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductPageComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  productObs$: Observable<any>;
 
   ngOnInit() {
+    this.productObs$ = this.apiService.getProductById(
+      "96d600ec-c2db-4dfd-a470-6dc775575e86"
+    );
   }
-
 }
