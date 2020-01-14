6c4e19fded6f063126fcb787385e4d05b84f7e14
Stephanie Zeng <stephanie.zeng@rangle.io>
2020-01-14 12:03:42 -0500

    #9 Add image uploader for product form

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../shared/services/api.service';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.dev';
 
 @Component({
   selector: 'os-create-product',
@@ -22,12 +23,29 @@ export class CreateProductComponent implements OnInit, OnDestroy {
     origin: ['', Validators.required]
   });
 
+  selectedImage = null;
+
   constructor(private fb: FormBuilder, private apiService: ApiService) {}
 
   ngOnInit() {}
 
   ngOnDestroy() {
     this.createProductStatus$.unsubscribe();
+  }
+
+  onSelectImage(event) {
+    this.selectedImage = event.target.files[0];
+  }
+
+  onUploadImage() {
+    const formData = new FormData();
+    formData.append('file', this.selectedImage);
+    formData.append('upload_preset', environment.CLOUDINARY_CONFIG.PRESET);
+    this.apiService.uploadImage(formData).subscribe((res) => {
+      if (!!res.url) {
+        this.createProductForm.get('imgUrl').setValue(res.url);
+      }
+    });
   }
 
   onSubmit() {
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/Product';
+import { environment } from 'src/environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   static baseUrl = 'http://localhost:3000/api';
+  static cloudinaryBaseUrl = `https://api.cloudinary.com/v1_1/${environment.CLOUDINARY_CONFIG.USER}/image/upload`;
 
   constructor(private http: HttpClient) {}
 
@@ -21,5 +23,11 @@ export class ApiService {
 
   public createProduct(body: Product): Observable<Product> {
     return this.http.post<Product>(`${ApiService.baseUrl}/products`, body);
+  }
+
+  public uploadImage(body): Observable<any> {
+    return this.http.post(ApiService.cloudinaryBaseUrl, body, {
+      observe: 'body'
+    });
   }
 }
