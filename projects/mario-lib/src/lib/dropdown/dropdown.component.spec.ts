ae4f357091f9fd219be9db8b1fc6f587ec8846d3
Stephanie Zeng <stephanie.zeng@rangle.io>
2020-02-19 11:45:43 -0500

    #17 Updated Angular material to V9

--- projects/mario-lib/src/lib/dropdown/dropdown.component.spec.ts
+++ projects/mario-lib/src/lib/dropdown/dropdown.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DropdownComponent } from './dropdown.component';
-import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DropdownComponent', () => {
--- projects/mario-lib/src/lib/dropdown/dropdown.module.ts
+++ projects/mario-lib/src/lib/dropdown/dropdown.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from './dropdown.component';
 import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [DropdownComponent],
