317c38b4144aefe6af586e1ba7e3169da108ffec
Stephanie Zeng <stephanie.zeng@rangle.io>
2020-06-24 21:41:53 -0400

    Add categories to header

--- src/app/app-shell/masthead/masthead.component.ts
+++ src/app/app-shell/masthead/masthead.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CategoryService } from 'src/app/shared/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/shared/models/Category';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'os-masthead',
   templateUrl: './masthead.component.html',
   styleUrls: ['./masthead.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MastheadComponent implements OnInit {
   title = 'Mario Odyssey Shop';
+  categoryObs$: Observable<Category[]>;
 
-  constructor() {}
+  constructor(private categoryService: CategoryService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.categoryObs$ = this.categoryService.getCategories().pipe(shareReplay(1));
+  }
 }
